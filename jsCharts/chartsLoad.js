9dcab82819d900f83cec1de0ebf9fb41f129c46a
Sam gunter <samgunter12@gmail.com>
2018-08-30 21:04:58 -0400

    Update
    Final site -data (hopefully)

--- jsCharts/chartsLoad.js
+++ jsCharts/chartsLoad.js
@@ -1,4 +1,361 @@
-FusionCharts.ready(function() { //Week12 Mobile
+
+FusionCharts.ready(function() { //Mobile
+	var revenueChart = new FusionCharts({
+		"type": "msline",
+		"renderAt": "mobSummaryChart",
+		"height": "100%",
+		"width": "100%",
+		"dataFormat": "json",
+		"dataSource": {
+			"chart": {
+				"caption": "% that won at each achievement level",
+				"subCaption": "Over course of competition",
+				"yAxisName": "Win %",
+				"xAxisName": "Week of contest",
+				"yAxisMaxValue": "100",
+				"yAxisNameFontSize": "12",
+				"xAxisNameFontSize": "12",
+				"theme": "ocean",
+				"showValues": "0",
+				"plottooltext": "$seriesname",
+				"drawAnchors": "0",
+				"showLegend": "0",
+				"canvasBgColor": "#eeeeee"
+			},
+			"categories": [
+				{
+					"category": [
+						{
+							"label": "1st"
+						},{
+							"label": "2nd"
+						},{
+							"label": "3rd"
+						},{
+							"label": "4th"
+						},{
+							"label": "5th"
+						},{
+							"label": "6th"
+						},{
+							"label": "7th"
+						},{
+							"label": "8th"
+						},{
+							"label": "9th"
+						},{
+							"label": "10th"
+						},{
+							"label": "11th"
+						},{
+							"label": "12th"
+						}
+					]
+				}
+			],
+			"dataset": [ //0
+				{
+					"seriesname": "0",
+					"color": "#000000",
+					"data": [
+						{
+							"value": "0"
+						},{
+							"value": "0"
+						},{
+							"value": "0"
+						},{
+							"value": "0"
+						},
+					]
+				},{
+					"seriesname": "1",
+					"color": "#000000",
+					"data": [
+						{
+							"value": "12"
+						},{
+							"value": "14"
+						},{
+							"value": "12"
+						},{
+							"value": "14"
+						},
+					]
+				},{
+					"seriesname": "2",
+					"color": "#003200",
+					"data": [
+						{
+							"value": "15"
+						},{
+							"value": "16"
+						},{
+							"value": "13"
+						},{
+							"value": "17"
+						},
+					]
+				},{
+					"seriesname": "3",
+					"color": "#005f00",
+					"data": [
+						{
+							"value": "20"
+						},{
+							"value": "19"
+						},{
+							"value": "21"
+						},{
+							"value": "23"
+						},
+					]
+				},{
+					"seriesname": "4",
+					"color": "#007800",
+					"data": [
+						{
+							"value": "23"
+						},{
+							"value": "22"
+						},{
+							"value": "26"
+						},{
+							"value": "25"
+						},
+					]
+				},{
+					"seriesname": "5",
+					"color": "#008c00",
+					"data": [
+						{
+							"value": "27"
+						},{
+							"value": "30"
+						},{
+							"value": "29"
+						},{
+							"value": "28"
+						},
+					]
+				},{
+					"seriesname": "6",
+					"color": "#00a000",
+					"data": [
+						{
+							"value": "31"
+						},{
+							"value": "31"
+						},{
+							"value": "32"
+						},{
+							"value": "33"
+						},
+					]
+				},{
+					"seriesname": "7",
+					"color": "#00b900",
+					"data": [
+						{
+							"value": "34"
+						},{
+							"value": "33"
+						},{
+							"value": "35"
+						},{
+							"value": "34"
+						},
+					]
+				},{
+					"seriesname": "8",
+					"color": "#00c800",
+					"data": [
+						{
+							"value": "37"
+						},{
+							"value": "38"
+						},{
+							"value": "37"
+						},{
+							"value": "36"
+						},
+					]
+				},{
+					"seriesname": "9",
+					"color": "#00dc00",
+					"data": [
+						{
+							"value": "40"
+						},{
+							"value": "41"
+						},{
+							"value": "42"
+						},{
+							"value": "44"
+						},
+					]
+				},{
+					"seriesname": "10",
+					"color": "#00eb00",
+					"data": [
+						{
+							"value": "46"
+						},{
+							"value": "48"
+						},{
+							"value": "51"
+						},{
+							"value": "49"
+						},
+					]
+				},{
+					"seriesname": "11",
+					"color": "#00ff00",
+					"data": [
+						{
+							"value": "52"
+						},{
+							"value": "55"
+						},{
+							"value": "53"
+						},{
+							"value": "53"
+						},
+					]
+				},{
+					"seriesname": "12",
+					"color": "#25ff25",
+					"data": [
+						{
+							"value": "56"
+						},{
+							"value": "55"
+						},{
+							"value": "58"
+						},{
+							"value": "59"
+						},
+					]
+				},{
+					"seriesname": "13",
+					"color": "#4bff4b",
+					"data": [
+						{
+							"value": "60"
+						},{
+							"value": "61"
+						},{
+							"value": "59"
+						},{
+							"value": "62"
+						},
+					]
+				},{
+					"seriesname": "14",
+					"color": "#64ff64",
+					"data": [
+						{
+							"value": "65"
+						},{
+							"value": "65"
+						},{
+							"value": "64"
+						},{
+							"value": "65"
+						},
+					]
+				},{
+					"seriesname": "15",
+					"color": "#7dff7d",
+					"data": [
+						{
+							"value": "67"
+						},{
+							"value": "66"
+						},{
+							"value": "65"
+						},{
+							"value": "68"
+						},
+					]
+				},{
+					"seriesname": "16",
+					"color": "#96ff96",
+					"data": [
+						{
+							"value": "81"
+						},{
+							"value": "78"
+						},{
+							"value": "80"
+						},{
+							"value": "80"
+						},
+					]
+				},{
+					"seriesname": "17",
+					"color": "#afffaf",
+					"data": [
+						{
+							"value": "84"
+						},{
+							"value": "85"
+						},{
+							"value": "83"
+						},{
+							"value": "82"
+						},
+					]
+				},{
+					"seriesname": "18",
+					"color": "#c8ffc8",
+					"data": [
+						{
+							"value": "87"
+						},{
+							"value": "86"
+						},{
+							"value": "87"
+						},{
+							"value": "88"
+						},
+					]
+				},{
+					"seriesname": "19",
+					"color": "#e1ffe1",
+					"data": [
+						{
+							"value": "92"
+						},{
+							"value": "94"
+						},{
+							"value": "94"
+						},{
+							"value": "95"
+						},
+					]
+				},{
+					"seriesname": "20",
+					"color": "#ffffff",
+					"data": [
+						{
+							"value": "99"
+						},{
+							"value": "100"
+						},{
+							"value": "97"
+						},{
+							"value": "98"
+						},
+					]
+				},
+			],
+		}
+	});
+	revenueChart.render();
+}) 
+
+FusionCharts.ready(function() { //Week12
 	var revenueChart = new FusionCharts({
 		"type": "pie2d",
 		"renderAt": "mobWinsWeek12",
@@ -3964,7 +4321,7 @@ FusionCharts.ready(function() {
 FusionCharts.ready(function() { //Desktop
 	var revenueChart = new FusionCharts({
 		"type": "msline",
-		"renderAt": "summaryChart",
+		"renderAt": "deskSummaryChart",
 		"height": "100%",
 		"width": "100%",
 		"dataFormat": "json",
@@ -3981,7 +4338,7 @@ FusionCharts.ready(function() { //Desktop
 				"showValues": "0",
 				"plottooltext": "$seriesname",
 				"drawAnchors": "0",
-				"bgColor": "#f0f0f0"
+				"canvasBgColor": "#eeeeee"
 			},
 			"categories": [
 				{
@@ -4315,7 +4672,6 @@ FusionCharts.ready(function() { //Desktop
 	});
 	revenueChart.render();
 }) 
-
 
 FusionCharts.ready(function() { //Week12
 	var revenueChart = new FusionCharts({
